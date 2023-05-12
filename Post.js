b5c608c13f674aaee3d7d9ba800dbe6f340d6b36
GinoGit <Gino@GinoGit.com>
2023-05-12 13:18:19 +0200

    first commit

---
+++ Post.js
@@ -0,0 +1,35 @@
+import { BskyAgent } from '@atproto/api';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const agent = new BskyAgent({
+  service: 'https://bsky.social',
+});
+
+start();
+
+async function start() {
+  try {
+    await agent.login({
+      identifier: process.env.BLUESKY_USERNAME,
+      password: process.env.BLUESKY_PASSWORD,
+    });
+
+    await post('Choo choo!');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function post(content) {
+  try {
+    const response = await agent.post({
+      text: content,
+    });
+
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
